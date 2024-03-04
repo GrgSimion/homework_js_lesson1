e18a9867f4135d1b57a20b837a59020845b34524
GrgSimion <grgsimion@gmail.com>
2024-03-04 20:43:52 +0200

    suma a doua numere

---
+++ script.js
@@ -0,0 +1,10 @@
+// ex 1
+let age = 39;
+console.log(` Varsta mea este ${age}`);
+
+// suma a doua numere
+let numberA = 12;
+let numberB = 21;
+let sum = numberA + numberB;
+console.log(` Rezultatul  adunarii dintre ${numberA} si ${numberB} este ${sum}`)
+
