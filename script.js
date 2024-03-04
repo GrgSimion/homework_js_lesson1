cf277c86e5ec0cad2f59f5a7223944fe0290b37f
GrgSimion <grgsimion@gmail.com>
2024-03-04 20:53:19 +0200

    elsius vs fahrenheit

--- script.js
+++ script.js
@@ -8,3 +8,11 @@ let numberB = 21;
 let sum = numberA + numberB;
 console.log(` Rezultatul  adunarii dintre ${numberA} si ${numberB} este ${sum}`)
 
+// celsius vs fahrenheit
+
+let  celsiusTemperature = 10;
+let multiplyResult= (celsiusTemperature * 9/5) + 32;
+console.log( `Rezultatul conversiei dintre Celsius & Fahreinheit este : ${multiplyResult}`)
+
+
+
